809c4aea811ad7ac8e9d0f966840951a3c52b44b
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-11-04 11:47:18 +0100

    IJBA

---
+++ bob/bio/face-ongoing/configs/databases/ijba/compare_split6.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+
+import bob.bio.face
+from bob.bio.face.database import IJBABioDatabase
+
+ijba_directory = "/idiap/resource/database/IJB-A/"
+
+database = IJBABioDatabase(
+  original_directory=ijba_directory,
+  protocol='compare_split6',
+  original_extension=".png"
+)
+
+sub_directory = 'ijba/compare/split6'
+
--- bob/bio/face-ongoing/configs/run.sh
+++ bob/bio/face-ongoing/configs/run.sh
@@ -6,17 +6,90 @@
 # -g 'grid' \
 
 
-./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split1.py \
+#./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split1.py \
+# --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+# --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+# -vvv \
+# -g 'grid'
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split2.py \
  --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
  --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
  -vvv \
- -g 'grid'
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
 
 
-#verify.py ./configs/baselines/vgg16.py ./configs/databases/ijba/search_split1.py \
-# --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
-# --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split3.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split4.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split5.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split6.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split7.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split8.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
+
+
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split9.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
 
 
 
-# -g 'grid' \
+./bin/verify.py ./bob/bio/face-ongoing/configs/baselines/vgg16.py ./bob/bio/face-ongoing/configs/databases/ijba/compare_split10.py \
+ --temp-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ --result-directory  /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA \
+ -vvv \
+ -g 'grid' \
+ --preprocessed-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/preprocessed \
+ --extracted-directory /idiap/temp/tpereira/bob.bio.face-ongoing/IJBA/ijba/compare/split1/extracted \
