d0e6f810195b19a2b9b7cb36c216d0b6a79e25b4
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-12-29 13:36:09 +0100

    Updated CASIA

--- bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/tfrecord_casia.sh
+++ bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/tfrecord_casia.sh
@@ -38,15 +38,15 @@
 
 ##
 
-./bin/jman submit --name CA-GRAY --repeat 1  --queue gpu -- \
- ./bin/bob_tf_train_generic \
- ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/CASIA_crossentropy_GRAY.py
-
-
-#./bin/jman submit --name E.CASIA-GRAY --queue q1d --io-big \
-# --environment="LD_LIBRARY_PATH=/idiap/user/tpereira/cuda/cuda-8.0/lib64:/idiap/user/tpereira/cuda/cudnn-8.0-linux-x64-v5.1/lib64:/idiap/user/tpereira/cuda/cuda-8.0" -- \
-#./bin/bob_tf_eval_generic \
+#./bin/jman submit --name CA-GRAY --repeat 1  --queue gpu -- \
+# ./bin/bob_tf_train_generic \
 # ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/CASIA_crossentropy_GRAY.py
+
+
+./bin/jman submit --name E.CASIA-GRAY --queue q1d --io-big \
+ --environment="LD_LIBRARY_PATH=/idiap/user/tpereira/cuda/cuda-8.0/lib64:/idiap/user/tpereira/cuda/cudnn-8.0-linux-x64-v5.1/lib64:/idiap/user/tpereira/cuda/cuda-8.0" -- \
+./bin/bob_tf_eval_generic \
+ ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/CASIA_crossentropy_GRAY.py
 
 
 
