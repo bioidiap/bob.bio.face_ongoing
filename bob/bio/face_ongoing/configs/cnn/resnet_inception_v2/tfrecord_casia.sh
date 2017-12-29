71d2624d383554d5cf53bd2ee00b0cb5c069e8cd
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-12-29 13:34:28 +0100

    Updated CASIA

--- bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/tfrecord_casia.sh
+++ bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/tfrecord_casia.sh
@@ -26,9 +26,9 @@
 
 ###
 
-./bin/jman submit --name CA-CROS --repeat 1  --queue gpu -- \
- ./bin/bob_tf_train_generic \
- ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/CASIA_crossentropy.py
+#./bin/jman submit --name CA-CROS --repeat 1  --queue gpu -- \
+# ./bin/bob_tf_train_generic \
+# ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/CASIA_crossentropy.py
 
 #./bin/jman submit --name E.CA-CROS --queue q1d --io-big \
 # --environment="LD_LIBRARY_PATH=/idiap/user/tpereira/cuda/cuda-8.0/lib64:/idiap/user/tpereira/cuda/cudnn-8.0-linux-x64-v5.1/lib64:/idiap/user/tpereira/cuda/cuda-8.0" -- \
