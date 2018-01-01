a0938044a5e3c87e6446067713659f27cbc194fc
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-01-01 18:25:59 +0100

    Added inceptionv1

--- bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/tfrecord_casia.sh
+++ bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/tfrecord_casia.sh
@@ -1,7 +1,10 @@
-#sleep 6300
 
-./bin/jman submit --name CASIA-v1-TT --repeat 1 --queue gpu \
-./bin/bob_tf_train_generic /idiap/user/tpereira/gitlab/bob/bob.bio.face-ongoing/bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/CASIA_cross_entropy_TAN-TRIGGS.py 
+./bin/jman submit --name CA-v1 --repeat 1 --queue gpu \
+./bin/bob_tf_train_generic ./bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/CASIA_centerloss.py
+
+
+#./bin/jman submit --name CASIA-v1-TT --repeat 1 --queue gpu \
+#./bin/bob_tf_train_generic /idiap/user/tpereira/gitlab/bob/bob.bio.face-ongoing/bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/CASIA_cross_entropy_TAN-TRIGGS.py
 #
 ###
 # EVALUATE
@@ -13,6 +16,12 @@
 # --environment="LD_LIBRARY_PATH=/idiap/user/tpereira/cuda/cuda-8.0/lib64:/idiap/user/tpereira/cuda/cudnn-8.0-linux-x64-v5.1/lib64:/idiap/user/tpereira/cuda/cuda-8.0" -- \
 #./bin/bob_tf_eval_generic \
 #/idiap/user/tpereira/gitlab/bob/bob.bio.face-ongoing/bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/CASIA_cross_entropy_TAN-TRIGGS.py
+
+
+#./bin/jman submit --name E.CA-v1 --queue q1d --io-big \
+# --environment="LD_LIBRARY_PATH=/idiap/user/tpereira/cuda/cuda-8.0/lib64:/idiap/user/tpereira/cuda/cudnn-8.0-linux-x64-v5.1/lib64:/idiap/user/tpereira/cuda/cuda-8.0" -- \
+#./bin/bob_tf_eval_generic \
+#./bob/bio/face_ongoing/configs/cnn/resnet_inception_v1/CASIA_centerloss.py
 
 
 
