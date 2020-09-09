34136e516cddf87f29368a1481b7b26bb91ab8e8
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-09-09 15:52:55 +0200

    Testing for tf2

--- bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/MSCeleba_centerloss.py
+++ bob/bio/face_ongoing/configs/cnn/resnet_inception_v2/MSCeleba_centerloss.py
@@ -1,90 +1,100 @@
-from bob.learn.tensorflow.network import inception_resnet_v2_batch_norm
-from bob.learn.tensorflow.estimators import LogitsCenterLoss
-from bob.learn.tensorflow.dataset.tfrecords import batch_data_and_labels_image_augmentation, shuffle_data_and_labels_image_augmentation
-from bob.learn.tensorflow.utils.hooks import LoggerHookEstimator
-from bob.learn.tensorflow.utils import reproducible
-import os
+#!/usr/bin/env python
+# coding: utf-8
+
 import tensorflow as tf
-
-# HYPER PARAMETERS
-learning_rate = 0.1
-data_shape = (182, 182, 3)  # size of atnt images
-output_shape = (160, 160)
-data_type = tf.uint8
-batch_size = 90
-#validation_batch_size = 50
-validation_batch_size = 38
-epochs = 5
-embedding_validation = True
-architecture=inception_resnet_v2_batch_norm
-
-alpha=0.90
-factor=0.02
-steps = 2000000
-
-model_dir = "/idiap/temp/tpereira/msceleb/inception_resnet_v2_rgb/centerloss_alpha-0.90_factor-0.02/"
+from glob import glob
+from tensorflow.keras import layers
+from bob.learn.tensorflow.models.inception_resnet_v2 import InceptionResNetV2
+from functools import partial
 
 
-# 10% PRUNNED
-#tf_record_path = "/idiap/project/hface/databases/tfrecords/msceleba/tfrecord_182x_hand_prunned_10%"
-#n_classes = 89288
+def main():
 
-# 20% PRUNNED
-#tf_record_path = "/idiap/project/hface/databases/tfrecords/msceleba/tfrecord_182x_hand_prunned_20/"
-#n_classes = 88526
+    train_tf_record_paths = sorted(
+        glob(
+            "/idiap/project/hface/databases/tfrecords/msceleba/"
+            "tfrecord_182x_hand_prunned_44/*.tfrecord"
+        )
+    )
+    N_CLASSES = 87662
+    DATA_SHAPE = (182, 182, 3)  # size of faces
+    OUTPUT_SHAPE = (160, 160)
+    DATA_TYPE = tf.uint8
+    TFRECORD_PARALLEL_READ = 4
+
+    SHUFFLE_BUFFER = int(2e4)
+
+    LEARNING_RATE = 0.1
+    BATCH_SIZE = 90
+    EPOCHS = 5
+
+    VALIDATION_BATCH_SIZE = 38
+    EMBEDDING_VALIDATION = True
+
+    DEFAULT_FEATURE = {
+        "data": tf.io.FixedLenFeature([], tf.string),
+        "label": tf.io.FixedLenFeature([], tf.int64),
+        "key": tf.io.FixedLenFeature([], tf.string),
+    }
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    def decode_tfrecords(x):
+        features = tf.io.parse_single_example(x, DEFAULT_FEATURE)
+        image = tf.io.decode_raw(features["data"], DATA_TYPE)
+        image = tf.reshape(image, DATA_SHAPE)
+        features["data"] = image
+        return features
+
+    preprocessing = tf.keras.Sequential(
+        [
+            layers.experimental.preprocessing.Rescaling(scale=1.0 / 255),  # 0 - 1
+            layers.experimental.preprocessing.RandomCrop(
+                height=OUTPUT_SHAPE[0], width=OUTPUT_SHAPE[1]
+            ),
+            layers.experimental.preprocessing.RandomFlip("horizontal"),
+            layers.experimental.preprocessing.RandomRotation(
+                5 / 360
+            ),  # math.pi * 5 /180
+            layers.experimental.preprocessing.Rescaling(
+                scale=1.0, offset=-0.5
+            ),  # -0.5 - 0.5
+        ]
+    )
+
+    def preprocess(features, training=True):
+        image = features["data"]
+        label = features["label"]
+        image = preprocessing(image, training=training)
+        return image, label
+
+    def prepare_dataset(tf_record_paths, shuffle=False, augment=False):
+        ds = tf.data.TFRecordDataset(
+            tf_record_paths, num_parallel_reads=TFRECORD_PARALLEL_READ
+        )
+        ds = ds.map(decode_tfrecords)
+        if shuffle:
+            ds = ds.shuffle(SHUFFLE_BUFFER)
+        ds = ds.batch(BATCH_SIZE).map(partial(preprocess, training=augment))
+
+        # Use buffered prefecting on all datasets
+        return ds.prefetch(buffer_size=AUTOTUNE)
+
+    train_ds = prepare_dataset(train_tf_record_paths, shuffle=True, augment=True)
+    train_ds
+
+    model = InceptionResNetV2(
+        include_top=True, classes=N_CLASSES, input_shape=OUTPUT_SHAPE + (3,)
+    )
+
+    model.compile(
+        optimizer=tf.keras.optimizers.RMSprop(
+            learning_rate=LEARNING_RATE, rho=0.9, momentum=0.9, epsilon=1.0
+        ),
+        loss=tf.keras.losses.sparse_categorical_crossentropy,
+    )
+
+    model.fit(train_ds, epochs=EPOCHS)
 
 
-# 44% PRUNNED
-tf_record_path = "/idiap/project/hface/databases/tfrecords/msceleba/tfrecord_182x_hand_prunned_44/"
-n_classes = 87662
-
-
-tf_record_path_validation = "/idiap/project/hface/databases/tfrecords/lfw/182x/RGB"
-
-# Creating the tf record
-#tfrecords_filename = [os.path.join(tf_record_path, f) for f in os.listdir(tf_record_path)]
-#tfrecords_filename_validation = [os.path.join(tf_record_path_validation, f) for f in os.listdir(tf_record_path_validation)]
-def train_input_fn():
-    return shuffle_data_and_labels_image_augmentation(tf_record_path, data_shape, data_type, batch_size, epochs=epochs,
-                                                      output_shape=output_shape,
-                                                      buffer_size=2*(10**4),
-                                                      random_flip=True,
-                                                      random_brightness=False,
-                                                      random_contrast=False,
-                                                      random_saturation=False,
-                                                      per_image_normalization=True,
-                                                      random_rotate=True,
-                                                      gray_scale=False)
-        
-
-def eval_input_fn():
-    return batch_data_and_labels_image_augmentation(tf_record_path_validation, data_shape, data_type, validation_batch_size, epochs=100000,
-                                                    output_shape=output_shape,
-                                                    random_flip=False,
-                                                    random_brightness=False,
-                                                    random_contrast=False,
-                                                    random_saturation=False,
-                                                    per_image_normalization=True,
-                                                    gray_scale=False)
-
-session_config, run_config,_,_,_ = reproducible.set_seed(log_device_placement=False)
-run_config = run_config.replace(save_checkpoints_steps=2000)
- 
-optimizer = tf.train.RMSPropOptimizer(learning_rate, decay=0.9, momentum=0.9, epsilon=1.0) 
-estimator = LogitsCenterLoss(model_dir=model_dir,
-                             architecture=architecture,
-                             optimizer=optimizer,
-                             n_classes=n_classes,
-                             embedding_validation=embedding_validation,
-                             validation_batch_size=validation_batch_size,
-                             alpha=alpha,
-                             factor=factor,
-                             config=run_config)
-
-
-hooks = [LoggerHookEstimator(estimator, 16, 300),
-         tf.train.SummarySaverHook(save_steps=1000,
-                                   output_dir=model_dir,
-                                   scaffold=tf.train.Scaffold(),
-                                   summary_writer=tf.summary.FileWriter(model_dir) )]
-
+if __name__ == "__main__":
+    main()
